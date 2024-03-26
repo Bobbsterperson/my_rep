b51825b61269d9924ff92725e1b5dc1a415dc23f
Bobbsterperson <156310921+Bobbsterperson@users.noreply.github.com>
2024-03-26 09:52:44 +0200

    Add files via upload

--- find_and_sort.py
+++ find_and_sort.py
@@ -1,13 +1,12 @@
 import os
 
 
-def find_dir():
-    current_directory = os.getcwd()
-    return current_directory
+def get_directory():
+    return os.getcwd()
 
 
 def list_items():
-    current_directory = find_dir()
+    current_directory = get_directory()
     directory = os.path.expanduser(current_directory)
     files = os.listdir(directory)
 
@@ -18,8 +17,10 @@ def get_data():
     pure_meta = []
 
     for n in list_items():
-        name = os.path.basename(n)
-        size = os.path.getsize(n)
+        full_path = os.path.join(get_directory(), n)
+        file_stat = os.stat(full_path)
+        name = os.path.basename(full_path)
+        size = file_stat.st_size
         pure_meta.append((name, size))
 
     return pure_meta
@@ -29,10 +30,23 @@ def sorted_data():
     return sorted(get_data(), key=lambda x: x[1])
 
 
+def create_text_file():
+    return open('sorted_metadata.txt', 'w')
+
+
+def write_to_file(file):
+    for item in sorted_data():
+        file.write(f"Name: {item[0]} - Size: {item[1]} bytes\n")
+
+
 def text_file():
-    with open('sorted_metadata.txt', 'w') as file:
-        for item in sorted_data():
-            file.write(f"Name: {item[0]} - Size: {item[1]} bytes\n")
+    file = create_text_file()
+    write_to_file(file)
 
 
-text_file()
+def main():
+    text_file()
+
+
+if __name__ == "__main__":
+    main()
