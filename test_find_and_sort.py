4f2f70ca56b101e2866a6ab028dbe01bb27d6e35
bugston <bugstonn@gmail.com>
2024-04-02 13:47:58 +0300

    testing

--- test_find_and_sort.py
+++ test_find_and_sort.py
@@ -7,33 +7,32 @@ from find_and_sort_pick_db_or_txt import parse_arguments
 class TestParseArguments(unittest.TestCase):
     
 
+    @patch('argparse.ArgumentParser.parse_args')
+    def test_t(self, mock_parse_args):
+        expected_args = argparse.Namespace(text = True,database = False)
+        mock_parse_args.return_value = expected_args
+        parsed_args = parse_arguments() 
+        self.assertEqual(parsed_args, "text")
+        
+
     @patch('argparse.ArgumentParser.parse_args')  #patches parse_args() from ArgumentParser class from argparse module
     def test_db(self, mock_parse_args):
-
-
-        expected_args = argparse.Namespace(db='-db')  # Nenespace is a placeholder for parsed arguments in terminal
+        expected_args = argparse.Namespace(database = True,text = False)  # Nenespace is a placeholder for parsed arguments in terminal
         mock_parse_args.return_value = expected_args
         parsed_args = parse_arguments()
-        self.assertEqual(parsed_args, expected_args)
-        if parsed_args.db == expected_args.db:
-            print("Test for -db: Passed")
-        else:
-            print("Test for -db: Failed")
+        self.assertEqual(parsed_args, "database")   #parsed args ==True not "database"
 
 
     @patch('argparse.ArgumentParser.parse_args')
-    def test_t_flag(self, mock_parse_args):
-        
-
-        expected_args = argparse.Namespace(t='-t')
+    def test_no_argument(self, mock_parse_args):
+        expected_args = argparse.Namespace(text = False,database = False)
         mock_parse_args.return_value = expected_args
-        parsed_args = parse_arguments()
-        self.assertEqual(parsed_args, expected_args)
-        if parsed_args.t == expected_args.t:
-            print("Test for -t flag: Passed")
-        else:
-            print("Test for -t flag: Failed")
+        try:
+            parsed_args = parse_arguments() 
+        except SystemExit as e:
+            self.assertEqual(e.code,2)
 
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
+    
